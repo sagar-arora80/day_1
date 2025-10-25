a8bbe368a7eb73c06c1e2d8c4b36c596807aa3c6
Sagar Arora <sagararora80@gmail.com>
2025-10-25 18:30:56 +0530

    Homepage v0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
 import './App.css';
+import HomePage from './views/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I love my boba to infinity and back
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomePage />
     </div>
   );
 }
-
 export default App;
+
